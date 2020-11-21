7e4ae899d3445a335643712f48014c85cd4eb0d7
Ahmed AlQahtani <alq.ahmad@gmail.com>
2020-11-21 17:28:50 +0300

    fix postcss configuration

--- postcss.config.js
+++ postcss.config.js
@@ -2,14 +2,18 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     "@fullhuman/postcss-purgecss":
-    {
-      content: [
-        "./pages/**/*.{js,jsx,ts,tsx}",
-        "./components/**/*.{js,jsx,ts,tsx}",
-      ],
-      defaultExtractor: (content) =>
-        content.match(/[\w-/:]+(?<!:)/g) || [],
-    },
+      process.env.NODE_ENV === "production"
+        ? {
+          // the paths to all template files
+          content: [
+            "./pages/**/*.{js,jsx,ts,tsx}",
+            "./components/**/*.{js,jsx,ts,tsx}",
+          ],
+          // function used to extract class names from the templates
+          defaultExtractor: (content) =>
+            content.match(/[\w-/:]+(?<!:)/g) || [],
+        }
+        : false,
     autoprefixer: {},
   }
 }
